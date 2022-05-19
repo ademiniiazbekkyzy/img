a5fd66936b70be4b4de82e45a6a2dfbad7c807db
ademiniiazbekkyzy <ademi.niiazbekkyzy@mail.ru>
2022-05-19 14:04:49 +0600

    added watermark

---
+++ README.md
@@ -0,0 +1,13 @@
+# images
+
+---
+
+A simple interactive tool for cropping images, adding a b/w filter and watermark.
+
+You can add an image to the 'images' folder and run it. The updated images will appear in the ‘updated images’ folder.
+
+Install dependencies using:
+
+```
+pip install -r requirements.txt
+```
--- main.py
+++ main.py
@@ -1,27 +1,62 @@
-from PIL import Image
-import os
+import os.path
 
-if not 'ltl' in os.listdir():
-    os.mkdir('ltl')
+from PIL import Image, ImageDraw, ImageFont
 
-# CROP, b/w filter
-# for i in os.listdir('.'):
-#     if i.endswith('.jpg'):
-#         img = Image.open(i)
-#         width, height = img.size
-#         left = (width - 1080) / 2
-#         top = (height - 1080) / 2
-#         right = (height + 1080) / 2
-#         bottom = (height + 1080) / 2
-#         img = img.convert(mode='L').crop((left, top, right, bottom))
-#         img.save('ltl\\' + i[:-4] + '.jpg')
+path = './images'
+new_path = './updated images//'
+dirs = os.listdir(path)
 
 
-# RESIZE, b/w filter
+def crop():
+    for i in dirs:
+        fullpath = os.path.join(path, i)
+        if os.path.isfile(fullpath):
+            image = Image.open(fullpath)
+            file_name = os.path.basename(fullpath)
+            f, e = os.path.splitext(file_name)
 
-for i in os.listdir('.'):
-    if i.endswith('.jpg'):
-        img = Image.open(i)
+            width, height = image.size
+            new_width, new_height = 1080, 1080
 
-        img = img.convert(mode='L').resize((1080, 1080))
-        img.save('ltl\\' + i[:-4] + '.jpg')
+            left = (width - new_width) / 2
+            top = (height - new_height) / 2
+            right = (width + new_width) / 2
+            bottom = (height + new_height) / 2
+
+            new_image = image.crop((left, top, right, bottom))
+            new_image.save(new_path + f + '.jpg', "JPEG", quality=100)
+
+
+crop()
+
+water_path = './updated images//'
+water_dirs = os.listdir(water_path)
+
+
+def watermark():
+    for i in water_dirs:
+        fullpath = os.path.join(water_path, i)
+        if os.path.isfile(fullpath):
+            image = Image.open(fullpath)
+            file_name = os.path.basename(fullpath)
+            f, e = os.path.splitext(file_name)
+
+            width, height = image.size
+
+            draw = ImageDraw.Draw(image)
+            text = "AUSTIN TEXAS"
+
+            font = ImageFont.truetype('Arial.ttf', 40)
+            text_width, text_height = draw.textsize(text, font)
+
+            margin = 50
+            x = width - text_width - margin
+            y = height - text_height - margin
+
+            draw.text((x, y), text, font=font)
+
+            image.save(water_path + f + '.jpg', 'JPEG')
+
+
+watermark()
+
